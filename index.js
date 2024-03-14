5503967f71d87bc27db5e3bdcf18d4f767bb5cc7
Brandon <brandonslee3@gmail.com>
2024-03-14 15:21:35 -0400

    completed stretch deliverables

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
 const formSubmit = document.querySelector("form#create-task-form")
 const newTask = document.getElementById("new-task-description")
-
+const dueDate = document.getElementById("new-task-due-date")
 
 formSubmit.addEventListener("submit", event => {
     event.preventDefault()
+    // Grabs input text and creates a task list
     const list = document.getElementById("tasks")
     const task = document.createElement("li")
-    task.textContent = newTask.value
+    console.log(dueDate.value)
+    if (dueDate != null) {
+        task.textContent = `${newTask.value} due on ${dueDate.value}`
+    }
+    else{
+        task.textContent = `${newTask.value}`
+    }
+
+    task.setAttribute("contenteditable", "true")
+    
+    // Updates color based on priority value
     const priorityValue = event.target.querySelector("#priority").value
-    // switch (priorityValue){
-    //     case "High":
-    //         task.fontcolor("red")
-    //         break
-    //     case "Medium":
-    //         task.fontcolor("yellow")
-    //         break
-    //     case "Low":
-    //         task.fontcolor("green")
-    //         break
-    // }
+    switch (priorityValue){
+        case "High":
+            task.style.color = "red"
+            break
+        case "Medium":
+            task.style.color = "yellow"
+            break
+        case "Low":
+            task.style.color = "green"
+            break
+    }
     list.append(task)
+
+    // Creates a remove button
     const removeButton = document.createElement("button")
-    removeButton.textContent = "X"
+    removeButton.textContent = "completed"
     task.append(removeButton)
+    // On the event that the button was pressed deletes the entire item off list
     removeButton.addEventListener("click", event => {
         task.remove()
     })
